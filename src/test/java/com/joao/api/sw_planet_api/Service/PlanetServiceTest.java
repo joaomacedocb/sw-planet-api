38ef77ec257787aebe873a1aa509ff41b56c591f
joaomacedocb <joaomacedocb@gmail.com>
2024-10-02 20:23:02 -0300

    Adicionado teste da buscar do planeta pelo id

--- src/test/java/com/joao/api/sw_planet_api/Service/PlanetServiceTest.java
+++ src/test/java/com/joao/api/sw_planet_api/Service/PlanetServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -50,6 +52,17 @@ public class PlanetServiceTest {
         assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
 
         
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet(){
+        when(planetRepository.findById(1L)).thenReturn(Optional.of(PLANET));
+
+        Optional<Planet> sut = planetService.get(1L);
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut.get()).isEqualTo(PLANET);
+
     }
     
 }
