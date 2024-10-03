f607bdff8e3abb77cb9f65a3dfd2601da12e88b9
joaomacedocb <joaomacedocb@gmail.com>
2024-10-03 07:21:23 -0300

    Adicionado testes para listagem de planeta

--- src/test/java/com/joao/api/sw_planet_api/Service/PlanetServiceTest.java
+++ src/test/java/com/joao/api/sw_planet_api/Service/PlanetServiceTest.java
@@ -2,8 +2,12 @@ package com.joao.api.sw_planet_api.Service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -11,8 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
 import com.joao.Service.PlanetService;
+import com.joao.Service.QueryBuilder;
 import com.joao.api.sw_planet_api.Domain.Planet;
 import com.joao.api.sw_planet_api.Repository.PlanetRepository;
 
@@ -84,4 +90,33 @@ public class PlanetServiceTest {
 
         assertThat(sut).isEmpty();
     }
+
+    @Test
+    public void listPlanets_returnAllPlanets(){
+        List<Planet> planets = new ArrayList<>(){
+            {
+            add(PLANET);
+        }
+    };
+
+    Example<Planet> query = QueryBuilder.makeQuery(new Planet(PLANET.getClimate(), PLANET.getTerrain()));
+    when(planetRepository.findAll(query)).thenReturn(planets);
+
+    List<Planet> sut = planetService.list(PLANET.getClimate(), PLANET.getTerrain());
+
+    assertThat(sut).isNotEmpty();
+    assertThat(sut).hasSize(1);
+    assertThat(sut.get(0)).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void listPlanets_ReturnEmptyList(){
+        when(planetRepository.findAll(any())).thenReturn(Collections.emptyList());
+
+        List<Planet> sut = planetService.list(PLANET.getClimate(), PLANET.getTerrain());
+
+        assertThat(sut).isEmpty();
+    }
+
+
 }
